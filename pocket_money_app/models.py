1901ce99f795898dde068c3d07703abe0fd55e41
Celia Stirling <celia.mackie@yahoo.co.uk>
2023-09-11 21:25:18 +0100

    created models

--- pocket_money_app/models.py
+++ pocket_money_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Child(models.Model):
+  firstname = models.CharField(max_length=30)
+  lastname = models.CharField(max_length=30)
+
+class Money_pot(models.Model):
+  child = models.ForeignKey(Child, on_delete=models.CASCADE)
+  balance = models.IntegerField()
+
+class Task(models.Model):
+  child = models.ForeignKey(Child, on_delete=models.CASCADE)
+  date_created = models.DateField(auto_now_add=True)
+  details = models.CharField(max_length=255)
+  value = models.IntegerField()
+  completed = models.BooleanField(default=False)
