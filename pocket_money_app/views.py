f330cf3deac294d90970eb9512934b716944417a
Celia Stirling <celia.mackie@yahoo.co.uk>
2023-09-12 21:48:10 +0100

    added children to db and updated pocket_money.html and views.py to show them on homepage

--- pocket_money_app/views.py
+++ pocket_money_app/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Child
 
 # Create your views here.
 
 def index(request):
+  children = Child.objects.all().values()
   template = loader.get_template('pocket_money.html')
-  return HttpResponse(template.render())
+  context = {
+    'children': children,
+  }
+  return HttpResponse(template.render(context, request))
